874fef44dc46752b38e31ddc6af607415847fab6
kimud6003 <kimud6003@gmail.com>
2023-06-04 19:40:12 +0900

    - themeProvider, GlobalStyle지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './Styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './Styles/theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme} />
+      <div className='App'></div>
+    </>
   );
 }
 
