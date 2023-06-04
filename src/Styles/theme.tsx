478bd1bc901953b34bfeabc7eccf3cd7580f682d
kimud6003 <kimud6003@gmail.com>
2023-06-04 19:39:55 +0900

    🎨 theme Color set

---
+++ src/Styles/theme.tsx
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  black: '#1e1f1d',
+  yellow: '#f9d71c',
+  oragne: '#ff9f1c',
+  blue: '#2d95ed',
+  white: '#ffffff',
+};
+
+export const theme: DefaultTheme = {
+  colors,
+};
